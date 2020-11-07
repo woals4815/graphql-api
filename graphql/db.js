371460f9822184b6a448980adc8f17e52479b8fb
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-08 00:09:35 +0900

    make fake DB

---
+++ graphql/db.js
@@ -0,0 +1,37 @@
+export const people = [
+    {
+        id: "0",
+        name: "Nicolas",
+        age: 18,
+        gender: "male"
+    },
+    {
+        id: "1",
+        name: "fuck",
+        age: 23,
+        gender: "female"
+    },
+    {
+        id: "2",
+        name: "slut",
+        age: 21,
+        gender: "female"
+    },
+    {
+        id: "3",
+        name: "pussy",
+        age: 25,
+        gender: "female"
+    },
+    {
+        id: "4",
+        name: "dickhead",
+        age: 32,
+        gender: "male"
+    }
+];
+
+export const getById = id => {
+    const filteredPeople = people.filter(person => people.id === id);
+    return filteredPeople[0];
+};
