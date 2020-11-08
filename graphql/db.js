265ab0016733b0b7557c42d85bc84afb3b66560a
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-08 14:23:13 +0900

    fetch API_URL from YTS

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,6 @@
-let movies = [
-    {
-        id: 0,
-        name: "Star-wars, The new age",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers- fuck blackwidow",
-        score: 7
-    },
-    {
-        id: 2,
-        name: "slut",
-        score: 10
-    },
-    {
-        id: 3,
-        name: "pussy",
-        score: 9
-    }
-];
+import fetch from "node-fetch";
+const API_URL= "https://yts.mx/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-};
-
-export const deleteMovies = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    } else{
-        return false;
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie={
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`).then(res => res.json()).then(json => json.data.movies);
 };
